1b701898832f7918beb717151a246375377cb894
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-04-20 23:55:03 +0100

    Added test to swipe viewPager and click on top rated movie

--- app/src/androidTest/java/com/example/popularmovies/ShowDetailsActivityTest.java
+++ app/src/androidTest/java/com/example/popularmovies/ShowDetailsActivityTest.java
@@ -1,5 +1,9 @@
 package com.example.popularmovies;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.ContentResolver;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
@@ -11,12 +15,19 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 
+import com.example.popularmovies.injection.PopularMoviesApplication;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
 
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.intent.Intents.*;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
@@ -34,12 +45,28 @@ public class ShowDetailsActivityTest {
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<MainActivity>(
             MainActivity.class);
 
-    @Test
-    public void whenUserClickOnAPopularMovieThenDetailsAcitivityisDisplayed() throws InterruptedException {
 
+    @Test
+    public void whenUserClickOnAPopularMovieThenDetailsAcitivityIsDisplayed() throws InterruptedException {
+
+        clickOnMovieAndVerifyDetailsIsDisplayed();
+
+    }
+
+    @Test
+    public void whenUserClickOnTopRatedMovieThenDetailsActivityIsDisplayed() throws InterruptedException {
+
+        onView(withId(R.id.container)).perform(swipeLeft());
+
+        // Find a way to make espresso wait for swipe
+        Thread.sleep(200);
+
+        clickOnMovieAndVerifyDetailsIsDisplayed();
+    }
+
+    private void clickOnMovieAndVerifyDetailsIsDisplayed() {
         // click on first movie
-        onView(allOf(
-                withId(R.id.rv_movies_grid), isDisplayed()))
+        onView(allOf(allOf(isDisplayed(), withId(R.id.rv_movies_grid))))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
 
@@ -47,5 +74,14 @@ public class ShowDetailsActivityTest {
         onView(withId(R.id.floatingActionButton)).check(ViewAssertions.matches(isDisplayed()));
         //intended(IntentMatchers.hasComponent(MovieDetailsActivity.class.getName()));
 
+
+    }
+
+    public class TestPopularMoviesApplication extends PopularMoviesApplication {
+
+        @Override
+        public ContentResolver getContentResolver() {
+            return super.getContentResolver();
+        }
     }
 }
